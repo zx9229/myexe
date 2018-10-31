cbc6e817facc2eafec97a676e5e8ff52cf878a30
zx <zx@163.com>
2018-10-31 19:13:16 +0800

    2018-10-31 19:13:12 备份

--- wx/wx.py
+++ wx/wx.py
@@ -2,6 +2,8 @@ import json
 import urllib
 import threading
 import http.server
+import sys
+import getopt
 import datetime
 import itchat
 
@@ -35,33 +37,27 @@ class MyResquestHandler(http.server.BaseHTTPRequestHandler):
         return
 
     def do_POST(self):
-        data = self.rfile.read(int(self.headers['content-length']))
-        data = MyResquestHandler.bytes2str(data)
-        data = json.loads(data)
-        splitResult = urllib.parse.urlsplit(self.path)
-        if splitResult.path == "/sendToGroup":
-            result = self.sendToGroup(data)
-        elif splitResult.path == "/sendToUser":
-            result = self.sendToUser(data)
-        else:
-            result = {'ErrNo': -1, 'ErrMsg': 'unknown url'}
+        try:
+            data = self.rfile.read(int(self.headers['content-length']))
+            data = MyResquestHandler.bytes2str(data)
+            data = json.loads(data)
+            splitResult = urllib.parse.urlsplit(self.path)
+            #
+            if splitResult.path != "/send":
+                raise Exception("unknown url")
+            #
+            if data.get("To") == "user":
+                send_to_user(data["Msg"], data.get("Name"),
+                             data.get("NickName"), data.get("RemarkName"))
+            elif data.get("To") == "group":
+                send_to_group(data["Msg"], data.get("Name"))
+            else:
+                raise Exception("unknown value of field To")
+            #
+            result = {'ErrNo': 0, 'ErrMsg': ''}
+        except Exception as ex:
+            result = {'ErrNo': -1, 'ErrMsg': ex.__str__()}
         self.wfile.write(json.dumps(result).encode())
-
-    def sendToGroup(self, data):
-        try:
-            send_to_group(data["Msg"], data["Name"])
-            result = {'ErrNo': 0, 'ErrMsg': ''}
-        except Exception as ex:
-            result = {'ErrNo': -1, 'ErrMsg': ex.__str__()}
-        return result
-
-    def sendToUser(self, data):
-        try:
-            send_to_user(data["Msg"], data["Name"])
-            result = {'ErrNo': 0, 'ErrMsg': ''}
-        except Exception as ex:
-            result = {'ErrNo': -1, 'ErrMsg': ex.__str__()}
-        return result
 
 
 # #############################################################################
@@ -77,45 +73,63 @@ def myExitCallback():
 
 @itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True)
 def text_reply(msg):
-    print(msg)
+    # print(msg)
     d = {
-        "Text": msg.text,
-        "CreateTime": msg.createTime,
-        "IsAt": msg.isAt,
         "MsgId": msg.msgId,
+        "IsAt": msg.get("IsAt"),
+        "Text": msg.text,
+        "Content": msg.content,
+        "CreateTime": msg.createTime,
         "NickName": msg.user.nickName,
-        "PYQuanPin": msg.user.pYQuanPin
+        "RemarkName": msg.user.remarkName,
+        "PYQuanPin": msg.user.pYQuanPin,
+        "UserName": msg.user.UserName
     }
     return str(d)
 
 
-def send_to_user(message, name):
-    raise NotImplementedError()
+def send_to_user(msg, name=None, nName=None, rName=None):
+    users = itchat.search_friends(name=name, nickName=nName, remarkName=rName)
+    if not users:
+        raise Exception("user not found")
+    user = users[0]
+    user.send(msg)
 
 
-def send_to_group(message, name):
+def send_to_group(msg, name=None):
     '''
     itchat 转发指定的微信群、用户的发言到指定的群
     https://blog.csdn.net/zhizunyu2009/article/details/79126375
     该程序的主要功能是监控撤回消息，并且如果有消息撤回就会撤回的消息发送给你，以后再也不用担心看不到好友的撤回的消息了
     https://www.cnblogs.com/ouyangping/p/8453920.html
     '''
-    toGroup = None
-    itchat.search_chatrooms()
-    groups = itchat.get_chatrooms(update=True)
-    for group in groups:
-        if group.nickName == name:
-            toGroup = group.userName
-    if toGroup is None:
-        print("is_none")
-    else:
-        itchat.send(msg=message, toUserName=toGroup)
+    groups = itchat.search_chatrooms(name=name)
+    if not groups:
+        raise Exception("group not found")
+    group = groups[0]
+    group.send(msg)
 
 
+# #############################################################################
+
+
+def calc_args():
+    host = 'localhost'
+    port = 65535
+    opts, args = getopt.getopt(sys.argv[1:], "h:p:")
+    for op, value in opts:
+        if op == "-h":
+            host = value
+        if op == "-p":
+            port = int(value)
+    return host, port
+
+
+# curl -d '{"To":"group","Msg":"测试消息","Name":"群名"}'  http://localhost:65535/send
+# curl -d '{"To":"user" ,"Msg":"测试消息","Name":"模糊搜索的名字","NickName":"昵称","RemarkName":"备注姓名"}' http://localhost:65535/send
 if __name__ == "__main__":
-    # curl -d '{"Msg":"测试消息","Name":"ZX自用"}' http://127.0.0.1:8080/sendToGroup
-    # curl -d '{"Msg":"测试消息","Name":"ZX自用"}' http://127.0.0.1:8080/sendToUser
-    myServer = MyHttpServer('localhost', 8080)
+    host, port = calc_args()
+    myServer = MyHttpServer(host, port)
     myServer.serverBackground()
     #
     enableCmdQR = -1
